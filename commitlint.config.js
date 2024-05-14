81a5a0d312f5ca363a3facf36095500a06b19909
zhinian <1053690126@qq.com>
2024-05-14 17:49:49 +0800

    feat: Initial project

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "style",
+      ],
+    ],
+    "header-case": [0, "always"],
+    "subject-case": [0, "always"],
+  },
+};
